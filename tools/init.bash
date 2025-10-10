0d3a3367c1f25655915acc8c31a6d59f7a2cc047
Andrew Kornilov <akornilov@gmail.com>
2025-10-10 03:00:49 +0300

    Fix prompt text

--- tools/init.bash
+++ tools/init.bash
@@ -131,7 +131,7 @@ setup_containers() {
 
     if [ -d "$VOL_DIR" ]; then
         echo "The 'vol' directory exists:"
-        echo " - In case of a new install type 'y' to clear its contents. WARNING! This will remove all previous configuration files and stored data of the proxy-client."
+        echo " - In case of a new install type 'y' to clear its contents. WARNING! This will remove all previous configuration files and stored data."
         echo " - In case of an upgrade/installing a new application type 'n' (or press Enter)."
         read -p "Clear it now? (y/N): " CONFIRM
         echo ""
@@ -140,7 +140,7 @@ setup_containers() {
             rm -rf "${VOL_DIR:?}"/*
         fi
     fi
-    
+
     mkdir -p "${VOL_DIR}/authentik-app/media" && chown 1000 "${VOL_DIR}/authentik-app/media"
 
     echo "Starting containers..."
