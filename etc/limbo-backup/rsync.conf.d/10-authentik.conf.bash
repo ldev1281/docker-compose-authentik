10adcc9b6a976af949a0b6177fc305b8d6ee5d55
Andrew Kornilov <akornilov@gmail.com>
2025-10-15 03:52:57 +0300

    Add backup task with full restore (including network creation) Add requirements check (limbo-backup.bash and docker) Add network check and creation if not exists

---
+++ etc/limbo-backup/rsync.conf.d/10-authentik.conf.bash
@@ -0,0 +1,12 @@
+CMD_BEFORE_BACKUP="docker compose --project-directory /docker/authentik down"
+CMD_AFTER_BACKUP="docker compose --project-directory /docker/authentik up -d"
+
+CMD_BEFORE_RESTORE="docker compose --project-directory /docker/authentik down || true"
+CMD_AFTER_RESTORE=(
+"docker network create --driver bridge proxy-client-authentik || true"
+"docker compose --project-directory /docker/authentik up -d"
+)
+
+INCLUDE_PATHS=(
+  "/docker/authentik"
+)
